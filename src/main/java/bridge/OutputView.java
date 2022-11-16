d09a3dab89f74e115e627d75a9d63b96a1d933df
rhqudco <rhqudco1204@naver.com>
2022-11-16 23:44:30 +0900

    REFACTOR: 시작할 때 출력 메서드 이동

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -20,4 +20,10 @@ public class OutputView {
      */
     public void printResult() {
     }
+
+    public static void printGameStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println("");
+        System.out.println("다리의 길이를 입력해 주세요.");
+    }
 }
--- src/main/java/bridge/PrintMessage.java
+++
@@ -1,9 +0,0 @@
-package bridge;
-
-public class PrintMessage {
-    public static void printGameStart() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
-        System.out.println("");
-        System.out.println("다리의 길이를 입력해 주세요.");
-    }
-}
